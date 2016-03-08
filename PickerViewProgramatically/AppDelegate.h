9b896455a82eebd202adc17bcff69a946ab17507
student <student@achiever96.local>
2016-03-08 19:11:25 +0530

    Initial Commit

---
+++ PickerViewProgramatically/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickerViewProgramatically
+//
+//  Created by student on 08/03/16.
+//  Copyright © 2016 BRN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PickerViewProgramatically/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PickerViewProgramatically
+//
+//  Created by student on 08/03/16.
+//  Copyright © 2016 BRN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
